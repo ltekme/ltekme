627bfcee434db1f4ca92dd7c8d13d97cad81fc12
Karl <karl@ltek.me>
2024-08-17 07:00:58 +0000

    add regex validation for terraform project template

--- Snippets-and-Templates/Terraform-Template/variables.tf
+++ Snippets-and-Templates/Terraform-Template/variables.tf
@@ -5,7 +5,11 @@ Default Variables
 variable "project-name" {
   description = "The name of the project(can only contain letters, numbers, and hyphens)"
   type        = string
-  default     = "Tamplate"
+  default     = "Template"
+  validation {
+    condition     = length(regexall("^[a-zA-Z0-9-]+$", var.project-name)) > 0
+    error_message = "project-name name must only contain letters, numbers, and hyphens."
+  }
 }
 
 variable "aws-region" {
